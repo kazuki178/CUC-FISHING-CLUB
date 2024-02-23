45adc117638dfe2943e9b941d02ec9564b2e4d48
kazuki178 <yahashi.kazuki0521@icloud.com>
2024-02-23 23:40:01 +0900

    targetのanimationを途中まで適応

--- animation/scrollAnimation.js
+++ animation/scrollAnimation.js
@@ -2,10 +2,16 @@
 ScrollReveal({ reset: true, distance: "50px", duration: 500 });
 
 
+//////////////////////////////
+//
+//  index.htmlのanimation
+//
+/////////////////////////////
+
 // logo headerのスライドanimation
 ScrollReveal().reveal('.logo, .header_menu', { delay: 400, origin: "left" });
 // image内テキストのcontact以外をスライドするanimation
-ScrollReveal().reveal('.image_inner .slide', { delay: 400, origin: "top" });
+ScrollReveal().reveal('.image_inner .pop', { delay: 400, origin: "top" });
 
 
 // section1の中の画像を使用性を下げない程度の時差付けでアニメーション表示
@@ -20,3 +26,7 @@ ScrollReveal().reveal('.section2 .container .row .col .image1', { delay: 400, or
 
 // image内テキストのcontact以外をスライドするanimation
 ScrollReveal().reveal('.footer .footer_inner h1,.footer .footer_inner p', { delay: 300, origin: "bottom" });
+
+
+
+
