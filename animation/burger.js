dfbabc2c2a073b71c8ad6e7a6739a397b8e2b6ce
kazuki178 <yahashi.kazuki0521@icloud.com>
2024-03-17 21:25:42 +0900

    jsの微調整

--- animation/burger.js
+++ animation/burger.js
@@ -2,17 +2,17 @@
 //バーガーメニューの開閉animation
 const burger = document.querySelector(".burger");
 const header = document.querySelector(".header_menu");
-const headerlinks=document.querySelectorAll(".header_menu li");
+const headerlinks = document.querySelectorAll(".header_menu li");
 
 burger.addEventListener("click", () => {
-  header.classList.toggle("header_active");
-  burger.classList.toggle("toggle");
+    header.classList.toggle("header_active");
+    burger.classList.toggle("toggle");
 
-  headerlinks.forEach((link,index) => {
-    if(link.style.animation){link.style.animation = "";
-    }  else{
-    link.style.animation = `headerlinksFade 0.5s ease forwards ${
-      index / 7 + 0.5}s`;
-    }
-  });
+    headerlinks.forEach((link, index) => {
+        if (link.style.animation) {
+            link.style.animation = "";
+        } else {
+            link.style.animation = `headerlinksFade 0.5s ease forwards ${index / 5 + 0.5}s`;
+        }
+    });
 });
